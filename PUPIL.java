8b211cecb287f9ed959d60acc35e5b38196457d9
Guest User <Guest@stk-cs01-ws15.westlothian.sch.uk>
2015-11-24 10:24:27 +0000

    sprint1.2
    Added list of properties

--- PUPIL.java
+++ PUPIL.java
@@ -1,11 +1,9 @@
 public class PUPIL
 {
-    
-    
-    
-    
-    
-    
+    //list of properties
+    private String fname;
+    private String sname;
+    private int pupilmark; 
     public PUPIL()
     {
       //constructor  
