7d54d6723588a5bdcc85ad55c27ca998d2a43cfa
emcelhinney <Guest@stk-cs01-ws15.westlothian.sch.uk>
2015-11-25 12:12:32 +0000

    sprint1.3
    finished constructor code and tested

--- PUPIL.java
+++ PUPIL.java
@@ -3,9 +3,34 @@ public class PUPIL
     //list of properties
     private String fname;
     private String sname;
-    private int pupilmark; 
+    private float pupilmark; 
     public PUPIL()
     {
-      //constructor  
+      // initialise instance variables
+      fname = "";
+      sname = "";
+      pupilmark = 0.0f;
+    }
+    // file handling store details from file
+    public void readPupilDetails(String dataItems)
+    {
+        // unpack string of row data into fileds
+        String[] rowItems = dataItems.split(",");
+        // store each data item as instance property
+        fname = rowItems[0];
+        sname = rowItems[1];
+        pupilmark = Float.parseFloat(rowItems[2]);
+    }
+    public String writeDetails()
+    { 
+        // join up data into a string to output as a row
+        // use "," to seperate csv columns
+        String pupilData = "";
+        pupilData = pupilData.concat(fname);
+        pupilData = pupilData.concat(",");
+        pupilData = pupilData.concat(sname);
+        pupilData = pupilData.concat(",");
+        pupilData = pupilData.concat(Float.toString(pupilmark));
+        return pupilData;
     }
 }
