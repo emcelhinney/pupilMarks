cef497e133b7d27d0947cc3df0b6b296b79c07ed
emcelhinney <Guest@stk-cs01-ws14.westlothian.sch.uk>
2015-11-26 09:38:27 +0000

    Sprint 2.1
    created SCHOOL class and set up pupil list ad count pupil marks

---
+++ SCHOOL.java
@@ -0,0 +1,30 @@
+public class SCHOOL
+{
+    //array of PUPIL objects
+    private PUPIL memberList[];
+    //number of pupil to be called
+    int noOFPupils;
+    public SCHOOL()
+    {
+
+    }
+
+    // top level algorithm
+    public void processpupils()
+    {
+
+        setUpPupilList();
+        countpupilmark();
+
+    }
+
+    private void setUpPupilList()
+    {
+        // placeholder
+    }
+
+    public void countpupilmark()
+    {
+        // placeholder
+    }
+}
